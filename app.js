54cc067c6a8cca1ddc28ac756f618334aae68c40
Matheus Moreira <matheusmoreira.emp@gmail.com>
2025-01-25 14:56:19 -0300

    adicionar amigo no array e limpar campo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo () {
+    let tagAmigo = document.querySelector('input');
+    let amigo = tagAmigo.value;
+
+    if (amigo != "") {
+        amigos.push(amigo);
+        tagAmigo.value = "";
+
+    } else {
+        alert(`Por favor, insira um nome.`);
+    }
+}
