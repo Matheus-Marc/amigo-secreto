09e51064afeff8239203a5eabbce310a1262a4d0
Matheus Moreira <matheusmoreira.emp@gmail.com>
2025-01-25 15:12:31 -0300

    exibir lista de amigos

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 let amigos = [];
 
-function adicionarAmigo () {
+function adicionarAmigo() {
     let tagAmigo = document.querySelector('input');
     let amigo = tagAmigo.value;
 
     if (amigo != '') {
         amigos.push(amigo);
+        exibirLista();
         tagAmigo.value = '';
 
     } else {
         alert('Por favor, insira um nome.');
     }
+}
+
+function exibirLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '' ;
+
+    amigos.forEach((amigo) => {
+        let item = document.createElement('li');
+        lista.append(amigo,item);
+    })
 }
